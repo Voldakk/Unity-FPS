dfeaf7228235f6c78007779e191fdfef5837b320
Voldakk <voldakk@outlook.com>
2018-06-21 00:40:26 +0200

    HUD and reloading

--- Assets/Scripts/PlayerShooting.cs
+++ Assets/Scripts/PlayerShooting.cs
@@ -1,8 +1,12 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerShooting : MonoBehaviour
 {
-    public Weapon weapon;
+
+    public Weapon startingWeapon;
+    private Weapon currentWeapon;
+
     public Transform weaponHolder;
 
     public GameObject lineRendererPrefab;
@@ -11,36 +15,88 @@ public class PlayerShooting : MonoBehaviour
     public float lineTime;
     private float lineTimer;
 
-	// Use this for initialization
-	void Start ()
+    public Image hudWeaponIcon;
+    public Text hudWeaponAmmo;
+
+    private bool reloading = false;
+
+    void Awake()
     {
-        weapon.gameObject = Instantiate(weapon.prefab, weaponHolder);
-
-        lineRenderer = Instantiate(lineRendererPrefab, weapon.gameObject.transform.Find("BarrelEnd")).GetComponent<LineRenderer>();
-
+        lineRenderer = Instantiate(lineRendererPrefab).GetComponent<LineRenderer>();
         lineRenderer.enabled = false;
     }
+
+    void Start ()
+    {
+        SetWeapon(startingWeapon);
+
+        currentWeapon.magCurrent = currentWeapon.magSize;
+        UpdateHudAmmo();
+    }
 	
-	// Update is called once per frame
+    void SetWeapon(Weapon newWeapon)
+    {
+        if(currentWeapon != null)
+        {
+            Destroy(currentWeapon.gameObject);
+        }
+
+        currentWeapon = Instantiate(newWeapon);
+
+        currentWeapon.gameObject = Instantiate(currentWeapon.prefab, weaponHolder);
+
+        lineRenderer.transform.SetParent(currentWeapon.gameObject.transform.Find("BarrelEnd"), false);
+
+        hudWeaponIcon.sprite = currentWeapon.icon;
+        hudWeaponIcon.type = Image.Type.Simple;
+        hudWeaponIcon.preserveAspect = true;
+        UpdateHudAmmo();
+    }
+
 	void Update ()
     {
-        weapon.reloadTimer += Time.deltaTime;
-        weapon.fireTimer += Time.deltaTime;
+        currentWeapon.reloadTimer += Time.deltaTime;
+        currentWeapon.fireTimer += Time.deltaTime;
         lineTimer += Time.deltaTime;
+
+        if(reloading && currentWeapon.reloadTimer >= currentWeapon.reloadTime)
+        {
+            reloading = false;
+
+            currentWeapon.magCurrent = currentWeapon.magSize;
+            UpdateHudAmmo();
+        }
 
         if(lineTimer >= lineTime && lineRenderer.enabled)
         {
             lineRenderer.enabled = false;
         }
 
-        if (Input.GetMouseButton(0))
+        if (Input.GetMouseButton(0) && !reloading)
         {
-            if(weapon.fireTimer >= 60.0f / weapon.firerate)
+            if(currentWeapon.fireTimer >= 60.0f / currentWeapon.firerate && currentWeapon.magCurrent > 0)
             {
-                weapon.fireTimer = 0;
+                currentWeapon.fireTimer = 0;
+
+                currentWeapon.magCurrent--;
+
                 lineTimer = 0;
                 lineRenderer.enabled = true;
+
+                UpdateHudAmmo();
             }
         }
+
+        if(Input.GetKeyDown(KeyCode.R) && !reloading)
+        {
+            currentWeapon.reloadTimer = 0.0f;
+            reloading = true;
+            hudWeaponAmmo.text = "Reloading...";
+        }
 	}
+
+    void UpdateHudAmmo()
+    {
+        hudWeaponAmmo.text = currentWeapon.magCurrent + " / " + currentWeapon.magSize;
+    }
 }

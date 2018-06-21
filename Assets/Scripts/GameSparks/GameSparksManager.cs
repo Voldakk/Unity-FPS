548bde5c9a46e261b81113b8c90e846d4a9c62d4
Voldakk <voldakk@outlook.com>
2018-06-21 14:17:38 +0200

    GameSparks lobby

---
+++ Assets/Scripts/GameSparks/GameSparksManager.cs
@@ -0,0 +1,247 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+using GameSparks.Core;
+using GameSparks.Api.Messages;
+using GameSparks.Api.Responses;
+using GameSparks.RT;
+
+using System.Collections.Generic;
+
+public class GameSparksManager : MonoBehaviour
+{
+    /// <summary>The GameSparks Manager singleton</summary>
+    private static GameSparksManager instance = null;
+
+    /// <summary>This method will return the current instance of this class </summary>
+    public static GameSparksManager Instance()
+    {
+        if (instance != null)
+        {
+            // return the singleton if the instance has been setup
+            return instance; 
+        }
+        else
+        { 
+            // otherwise return an error
+            Debug.LogError("GSM| GameSparksManager Not Initialized...");
+        }
+        return null;
+    }
+
+    void Awake()
+    {
+        instance = this; // if not, give it a reference to this class...
+        DontDestroyOnLoad(this.gameObject); // and make this object persistent as we load new scenes
+    }
+
+    #region Login & Registration
+    public delegate void AuthCallback(AuthenticationResponse _authresp2);
+    public delegate void RegCallback(RegistrationResponse _authResp);
+    /// <summary>
+    /// Sends an authentication request or registration request to GS.
+    /// </summary>
+    /// <param name="_callback1">Auth-Response</param>
+    /// <param name="_callback2">Registration-Response</param>
+    public void AuthenticateUser(string _userName, string _password, RegCallback _regcallback, AuthCallback _authcallback)
+    {
+        new GameSparks.Api.Requests.RegistrationRequest()
+            // this login method first attempts a registration //
+            // if the player is not new, we will be able to tell as the registrationResponse has a bool 'NewPlayer' which we can check
+            // for this example we use the user-name was the display name also //
+            .SetDisplayName(_userName)
+            .SetUserName(_userName)
+            .SetPassword(_password)
+            .Send((regResp) =>
+            {
+                if (!regResp.HasErrors)
+                { // if we get the response back with no errors then the registration was successful
+                    Debug.Log("GSM| Registration Successful...");
+                    _regcallback(regResp);
+                }
+                else
+                {
+                    // if we receive errors in the response, then the first thing we check is if the player is new or not
+                    if (!(bool)regResp.NewPlayer) // player already registered, lets authenticate instead
+                    {
+                        Debug.LogWarning("GSM| Existing User, Switching to Authentication");
+                        new GameSparks.Api.Requests.AuthenticationRequest()
+                            .SetUserName(_userName)
+                            .SetPassword(_password)
+                            .Send((authResp) =>
+                            {
+                                if (!authResp.HasErrors)
+                                {
+                                    Debug.Log("Authentication Successful...");
+                                    _authcallback(authResp);
+                                }
+                                else
+                                {
+                                    Debug.LogWarning("GSM| Error Authenticating User \n" + authResp.Errors.JSON);
+                                }
+                            });
+                    }
+                    else
+                    {
+                        // if there is another error, then the registration must have failed
+                        Debug.LogWarning("GSM| Error Authenticating User \n" + regResp.Errors.JSON);
+                    }
+                }
+            });
+    }
+    #endregion
+
+    #region Matchmaking Request
+    /// <summary>
+    /// This will request a match between as many players you have set in the match.
+    /// When the max number of players is found each player will receive the MatchFound message
+    /// </summary>
+    public void FindPlayers()
+    {
+        Debug.Log("GSM| Attempting Matchmaking...");
+        new GameSparks.Api.Requests.MatchmakingRequest()
+            .SetMatchShortCode("1v1") // set the shortCode to be the same as the one we created in the first tutorial
+            .SetSkill(0) // in this case we assume all players have skill level zero and we want anyone to be able to join so the skill level for the request is set to zero
+            .Send((response) => {
+                if (response.HasErrors)
+                { // check for errors
+                    Debug.LogError("GSM| MatchMaking Error \n" + response.Errors.JSON);
+                }
+            });
+    }
+    #endregion
+
+    private GameSparksRTUnity gameSparksRTUnity;
+
+    public GameSparksRTUnity GetRTSession()
+    {
+        return gameSparksRTUnity;
+    }
+
+    private RTSessionInfo sessionInfo;
+
+    public RTSessionInfo GetSessionInfo()
+    {
+        return sessionInfo;
+    }
+
+    public void StartNewRTSession(RTSessionInfo _info)
+    {
+        Debug.Log("GSM| Creating New RT Session Instance...");
+        sessionInfo = _info;
+        gameSparksRTUnity = this.gameObject.AddComponent<GameSparksRTUnity>(); // Adds the RT script to the game
+        // In order to create a new RT game we need a 'FindMatchResponse' //
+        // This would usually come from the server directly after a successful MatchmakingRequest //
+        // However, in our case, we want the game to be created only when the first player decides using a button //
+        // therefore, the details from the response is passed in from the gameInfo and a mock-up of a FindMatchResponse //
+        // is passed in. //
+        GSRequestData mockedResponse = new GSRequestData()
+                                            .AddNumber("port", (double)_info.GetPortID())
+                                            .AddString("host", _info.GetHostURL())
+                                            .AddString("accessToken", _info.GetAccessToken()); // construct a dataset from the game-details
+
+        FindMatchResponse response = new FindMatchResponse(mockedResponse); // create a match-response from that data and pass it into the game-config
+        // So in the game-config method we pass in the response which gives the instance its connection settings //
+        // In this example, I use a lambda expression to pass in actions for 
+        // OnPlayerConnect, OnPlayerDisconnect, OnReady and OnPacket actions //
+        // These methods are self-explanatory, but the important one is the OnPacket Method //
+        // this gets called when a packet is received //
+
+        gameSparksRTUnity.Configure(response,
+            (peerId) => { OnPlayerConnectedToGame(peerId); },
+            (peerId) => { OnPlayerDisconnected(peerId); },
+            (ready) => { OnRTReady(ready); },
+            (packet) => { OnPacketReceived(packet); });
+        gameSparksRTUnity.Connect(); // when the config is set, connect the game
+
+    }
+
+    private void OnPlayerConnectedToGame(int _peerId)
+    {
+        Debug.Log("GSM| Player Connected, " + _peerId);
+    }
+
+    private void OnPlayerDisconnected(int _peerId)
+    {
+        Debug.Log("GSM| Player Disconnected, " + _peerId);
+    }
+
+    private void OnRTReady(bool _isReady)
+    {
+        if (_isReady)
+        {
+            Debug.Log("GSM| RT Session Connected...");
+            SceneManager.LoadScene("Main");
+        }
+
+    }
+
+    private void OnPacketReceived(RTPacket _packet)
+    {
+        switch (_packet.OpCode)
+        {
+            case 1:
+                // Chat
+                break;
+
+            case 2:
+                // Player position update
+                break;
+
+            case 3:
+                // Player fire
+                break;
+
+        }
+    }
+}
+
+public class RTSessionInfo
+{
+    private string hostURL;
+    public string GetHostURL() { return this.hostURL; }
+    private string acccessToken;
+    public string GetAccessToken() { return this.acccessToken; }
+    private int portID;
+    public int GetPortID() { return this.portID; }
+    private string matchID;
+    public string GetMatchID() { return this.matchID; }
+
+    private List<RTPlayer> playerList = new List<RTPlayer>();
+    public List<RTPlayer> GetPlayerList()
+    {
+        return playerList;
+    }
+
+    /// <summary>
+    /// Creates a new RTSession object which is held until a new RT session is created
+    /// </summary>
+    /// <param name="_message">Message.</param>
+    public RTSessionInfo(MatchFoundMessage _message)
+    {
+        portID = (int)_message.Port;
+        hostURL = _message.Host;
+        acccessToken = _message.AccessToken;
+        matchID = _message.MatchId;
+        // we loop through each participant and get their peerId and display name //
+        foreach (MatchFoundMessage._Participant p in _message.Participants)
+        {
+            playerList.Add(new RTPlayer(p.DisplayName, p.Id, (int)p.PeerId));
+        }
+    }
+
+    public class RTPlayer
+    {
+        public RTPlayer(string _displayName, string _id, int _peerId)
+        {
+            this.displayName = _displayName;
+            this.id = _id;
+            this.peerId = _peerId;
+        }
+
+        public string displayName;
+        public string id;
+        public int peerId;
+        public bool isOnline;
+    }
+}
---
+++ Assets/Scripts/GameSparks/LobbyManager.cs
@@ -0,0 +1,114 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using GameSparks.Core;
+using GameSparks.Api.Responses;
+using System.Text;
+using System.Linq;
+
+public class LobbyManager : MonoBehaviour
+{
+    public Text userId, connectionStatus;
+    public InputField userNameInput, passwordInput;
+    public GameObject loginPanel;
+    public Button loginBttn, matchmakingBttn, startGameBttn;
+    public Text matchDetails;
+    public GameObject matchDetailsPanel;
+
+    private RTSessionInfo tempRTSessionInfo;
+
+    void Start()
+    {
+
+        // we won't start with a user logged in so lets show this also
+        userId.text = "No User Logged In...";
+
+        // we won't immediately have connection, so at the start of the lobby we
+        // will set the connection status to show this
+        connectionStatus.text = "No Connection...";
+        GS.GameSparksAvailable += (isAvailable) => {
+            if (isAvailable)
+            {
+                connectionStatus.text = "GameSparks Connected...";
+            }
+            else
+            {
+                connectionStatus.text = "GameSparks Disconnected...";
+            }
+        };
+        // only the login panel and login button is needed at the start of the scene, so disable any other objects //
+        matchDetailsPanel.SetActive(false);
+        matchmakingBttn.gameObject.SetActive(false);
+        startGameBttn.gameObject.SetActive(false);
+        // we add a custom listener to the on-click delegate of the login button so we don't need to create extra methods //
+        loginBttn.onClick.AddListener(() => {
+            GameSparksManager.Instance().AuthenticateUser(userNameInput.text, passwordInput.text, OnRegistration, OnAuthentication);
+        });
+
+        matchmakingBttn.onClick.AddListener(() => {
+            GameSparksManager.Instance().FindPlayers();
+            matchDetails.text = "Searching For Players...";
+        });
+
+        // this listener will update the text in the player-list field if no match was found //
+        GameSparks.Api.Messages.MatchNotFoundMessage.Listener = (message) => {
+            matchDetails.text = "No Match Found...";
+        };
+
+        GameSparks.Api.Messages.MatchFoundMessage.Listener += OnMatchFound;
+
+        // this is a listener for the startGameBttn. Onclick, we will will pass the stored RTSessionInfo to the GameSparksManager to create a new RT session //
+        startGameBttn.onClick.AddListener(() => {
+            GameSparksManager.Instance().StartNewRTSession(tempRTSessionInfo);
+        });
+    }
+
+    // <summary>
+    /// this is called when a player is registered
+    /// </summary>
+    /// <param name="_resp">Resp.</param>
+    private void OnRegistration(RegistrationResponse _resp)
+    {
+        userId.text = "User ID: " + _resp.UserId;
+        connectionStatus.text = "New User Registered...";
+        loginPanel.SetActive(false);
+        loginBttn.gameObject.SetActive(false);
+        matchmakingBttn.gameObject.SetActive(true);
+        matchDetailsPanel.SetActive(true);
+    }
+    /// <summary>
+    /// This is called when a player is authenticated
+    /// </summary>
+    /// <param name="_resp">Resp.</param>
+    private void OnAuthentication(AuthenticationResponse _resp)
+    {
+        userId.text = "User ID: " + _resp.UserId;
+        connectionStatus.text = "User Authenticated...";
+        loginPanel.SetActive(false);
+        loginBttn.gameObject.SetActive(false);
+        matchmakingBttn.gameObject.SetActive(true);
+        matchDetailsPanel.SetActive(true);
+    }
+
+    private void OnMatchFound(GameSparks.Api.Messages.MatchFoundMessage _message)
+    {
+        Debug.Log("Match Found!...");
+        StringBuilder sBuilder = new StringBuilder();
+        sBuilder.AppendLine("Match Found...");
+        sBuilder.AppendLine("Host URL:" + _message.Host);
+        sBuilder.AppendLine("Port:" + _message.Port);
+        sBuilder.AppendLine("Access Token:" + _message.AccessToken);
+        sBuilder.AppendLine("MatchId:" + _message.MatchId);
+        sBuilder.AppendLine("Opponents:" + _message.Participants.Count());
+        sBuilder.AppendLine("_________________");
+        sBuilder.AppendLine(); // we'll leave a space between the player-list and the match data
+        foreach (GameSparks.Api.Messages.MatchFoundMessage._Participant player in _message.Participants)
+        {
+            sBuilder.AppendLine("Player:" + player.PeerId + " User Name:" + player.DisplayName); // add the player number and the display name to the list
+        }
+        matchDetails.text = sBuilder.ToString(); // set the string to be the player-list field
+
+        tempRTSessionInfo = new RTSessionInfo(_message); // we'll store the match data until we need to create an RT session instance
+        matchmakingBttn.gameObject.SetActive(false);
+        startGameBttn.gameObject.SetActive(true);
+    }
+}

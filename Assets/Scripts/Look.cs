96f80389558da3712d4d9b5e7460a08b3eb792bf
Voldakk <voldakk@outlook.com>
2018-06-29 00:50:28 +0200

    Standard assets FPS controller

---
+++ Assets/Scripts/Look.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Look : MonoBehaviour
+{
+    public float XSensitivity = 2f;
+    public float YSensitivity = 2f;
+
+    public Transform look;
+
+    private readonly float halfsqrt = Mathf.Sqrt(0.5f);
+
+    void Update ()
+    {
+        // Horizontal
+        float yRot = CrossPlatformInputManager.GetAxis("Mouse X") * XSensitivity;
+        transform.localRotation *= Quaternion.Euler(0f, yRot, 0f);
+
+        // Vertical
+        float xRot = -CrossPlatformInputManager.GetAxis("Mouse Y") * YSensitivity;
+        look.localRotation *= Quaternion.Euler(xRot, 0f, 0f);
+
+        float lookX = look.localRotation.x;
+        if (lookX < -halfsqrt || lookX > halfsqrt)
+        {
+            float x = Mathf.Clamp(lookX, -halfsqrt, halfsqrt);
+            float w = halfsqrt;
+
+            look.localRotation = new Quaternion(x, 0f, 0f, w);
+        }
+    }
+}
--- Assets/Standard Assets/Characters/FirstPersonCharacter/Scripts/FirstPersonController.cs
+++ Assets/Standard Assets/Characters/FirstPersonCharacter/Scripts/FirstPersonController.cs
@@ -17,6 +17,7 @@ namespace UnityStandardAssets.Characters.FirstPerson
         [SerializeField] private float m_JumpSpeed;
         [SerializeField] private float m_StickToGroundForce;
         [SerializeField] private float m_GravityMultiplier;
+        [SerializeField] private bool m_UseMouseLook;
         [SerializeField] private MouseLook m_MouseLook;
         [SerializeField] private bool m_UseFovKick;
         [SerializeField] private FOVKick m_FovKick = new FOVKick();
@@ -236,7 +237,8 @@ namespace UnityStandardAssets.Characters.FirstPerson
 
         private void RotateView()
         {
-            m_MouseLook.LookRotation (transform, m_Camera.transform);
+            if(m_UseMouseLook)
+                m_MouseLook.LookRotation (transform, m_Camera.transform);
         }
 
 
